a2b923c9a9950f299d6f95b3a84718f466dd9601
Wouter Admiraal <wouter.admiraal@sonarsource.com>
2018-12-13 08:36:14 +0100

    Update app

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
   entry: "./src/index.tsx",
-  mode: "development",
+  mode: "production",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
