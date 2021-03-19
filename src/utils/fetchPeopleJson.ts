d3e924c71950aecf543f73bb6c25c54081a94193
Wouter Admiraal <45544358+wouter-admiraal-sonarsource@users.noreply.github.com>
2021-03-19 11:56:53 +0100

    Update fetchPeopleJson.ts

--- src/utils/fetchPeopleJson.ts
+++ src/utils/fetchPeopleJson.ts
@@ -2,10 +2,14 @@ import { PersonItem } from './fetchPeople';
 
 export default function() {
   return new Promise((resolve, reject) => {
+    if (true) {
     fetch('/team.json')
       .then(response => response.json())
       .then(json => {
         resolve(json);
       });
+    } else {
+      return 12; 
+    }
   });
 }
