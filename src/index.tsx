58e25bc9b1aee388cfe2e3a841ffe30028899a77
Wouter Admiraal <45544358+wouter-admiraal-sonarsource@users.noreply.github.com>
2021-05-28 15:19:29 +0200

    Introduce error

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import * as ReactDOM from 'react-dom';
 
 import App from './app/App';
 
+if (true) {
+  console.log('yo');   
+}
+
 ReactDOM.render(
     <App />,
     document.getElementById('app')
